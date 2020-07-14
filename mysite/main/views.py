e9345d93f5d3122e5dd674c711326018d0da3b5d
Lok <loklok12614@gmail.com>
2020-07-13 23:45:24 -0500

    New form on list view/ checkbox for list view

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -30,6 +30,24 @@ def allList(response):
 
 def list(response, id):
     ls = ToDoList.objects.get(id=id)
+
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)):
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("Invalid")
     return render(response, "main/list.html", {"ls":ls})
 
 def home(response):
