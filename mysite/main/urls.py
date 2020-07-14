504551d3eb393794a2a797dba915a825ce57ef53
Lok <loklok12614@gmail.com>
2020-07-14 15:40:00 -0500

    bootstrap styles for website

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("list/", views.allList, name="allList"),
     path("list/<int:id>", views.list, name="list"),
     path("create/", views.create, name="create"),
